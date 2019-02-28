67e3e57e9d7277a9d4423102737a506f58adde75
Avinash Shenoy <ashenoy@akamai.com>
2019-02-28 11:52:32 -0800

    Adjust cmd argument parser to allow lower and upper bound for hosts to be scanned

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const sitesFile = "rum_migration_domains.csv";
 
 // command-line arguments
 if (process.argv.length <= 2) {
-    console.error("Usage: index.js [url | number of Alexa Top N sites in input file]");
+    console.error("Usage: index.js [url | start end] {(start,end) represent the start and end index from the host input list}");
     process.exit(1);
 }
 
@@ -24,11 +24,30 @@ if (process.argv[2].indexOf("http") === 0) {
 } else {
     // if given a nuber, load that many sites from the CSV
     const numberOfSites = process.argv[2];
+    let upperRangeOfIndex = undefined;
+    if (process.argv[3]) {
+    	upperRangeOfIndex = process.argv[3];
+    }
 
-    // load the sites
-    sites = fs.readFileSync(sitesFile, "utf-8")
-        .split("\n")
-        .slice(0, numberOfSites);
+    console.log("lower index: " + numberOfSites);
+    console.log("upperRangeOfIndex: " + upperRangeOfIndex);
+
+    if (!upperRangeOfIndex || (upperRangeOfIndex <= numberOfSites)) {
+		// load the sites assuming only upper bound specified on # of sites to process
+		console.log("Will process the first " + numberOfSites + " number of sites");
+		sites = fs.readFileSync(sitesFile, "utf-8")
+			.split("\n")
+			.slice(0, numberOfSites);
+
+    } else {
+    	// User wants to only checks host between lower and upper bound, with upper bound
+    	// not included.
+		// load the sites
+		console.log("Will process the sites in list starting at " + numberOfSites + " and up to but not including sites at index " + upperRangeOfIndex);
+		sites = fs.readFileSync(sitesFile, "utf-8")
+			.split("\n")
+			.slice(numberOfSites, upperRangeOfIndex);
+    }
 }
 
 // call Crawler
